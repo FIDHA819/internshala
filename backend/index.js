95f559a0fe00e0b550c1e1ca0811175b623105aa
unknown <fidhapichu461@gmail.com>
2026-05-21 11:49:48 +0600

    fix cors

--- backend/index.js
+++ backend/index.js
@@ -1,38 +1,53 @@
 const bodyparser = require("body-parser");
 const express = require("express");
+const cors = require("cors");
+
 const app = express();
 
 const { connect } = require("./db");
 const router = require("./Routes/index");
-const port = 5000;
 
-const cors = require("cors");
+const port = process.env.PORT || 5000;
 
+// CORS
 app.use(
   cors({
     origin: [
       "http://localhost:3000",
       "https://internshala-topaz.vercel.app",
     ],
+    methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
 );
 
-app.use(express.json());
-app.use(bodyparser.json({ limit: "50mb" }));
-app.use(bodyparser.urlencoded({ extended: true, limit: "50mb" }));
+// Middlewares
 app.use(express.json());
 
+app.use(
+  bodyparser.json({
+    limit: "50mb",
+  })
+);
+
+app.use(
+  bodyparser.urlencoded({
+    extended: true,
+    limit: "50mb",
+  })
+);
+
+// Routes
 app.get("/", (req, res) => {
   res.send("hello this is internshala backend");
 });
+
 app.use("/api", router);
+
+// DB
 connect();
-app.use((req, res, next) => {
-  req.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Origin", "*");
-  next();
-});
+
+// Server
 app.listen(port, () => {
-  console.log(`Server is running on the port ${port}`);
+  console.log(`Server running on ${port}`);
 });
