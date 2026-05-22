001231318c75ad27cf4c07abc87a236f91206b4d
FIDHA819 <fidhapichu461@gmail.com>
2026-05-22 11:51:07 +0600

    Update database connection URL to use DATABASE_URL

--- backend/db.js
+++ backend/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 async function connect() {
   await mongoose.connect(
-    process.env.MONGO_URL
+    process.env.DATABASE_URL
   );
 
   console.log("database is connected");
